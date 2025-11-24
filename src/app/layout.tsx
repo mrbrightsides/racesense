a498cf44e31de0c77e25464b7af415e1f8e6a7f7
Akhmad Khudri <khudri@binadarma.ac.id>
2025-11-24 21:51:02 +0700

    Update ReadyNotifier comment and button title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
           <body
             className="antialiased font-sans"
           >
-            {/* Do not remove this component, we use it to notify the parent that the mini-app is ready */}
+            {/* Core system component for application initialization */}
             <ReadyNotifier />
             
       <FarcasterWrapper>
@@ -36,6 +36,6 @@ export default function RootLayout({
 export const metadata: Metadata = {
         title: "Vercel Deployment Fixer",
         description: "Resolve Vercel deployment errors with missing module hooks and optimize your build process for seamless Next.js app updates and fixes.",
-        other: { "fc:frame": JSON.stringify({"version":"next","imageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/thumbnail_miniapp_cmid40lk90kww0arq8hmofhcd-yqJsWWuIdKIYs4QsdfWO5iDpS34TDF","button":{"title":"Open with Ohara","action":{"type":"launch_frame","name":"Vercel Deployment Fixer","url":"https://between-method-154.app.ohara.ai","splashImageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/farcaster/splash_images/splash_image1.svg","splashBackgroundColor":"#ffffff"}}}
+          other: { "fc:frame": JSON.stringify({"version":"next","imageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/thumbnail_miniapp_cmid40lk90kww0arq8hmofhcd-yqJsWWuIdKIYs4QsdfWO5iDpS34TDF","button":{"title":"ELPEEF","action":{"type":"launch_frame","name":"Vercel Deployment Fixer","url":"https://between-method-154.app.ohara.ai","splashImageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/farcaster/splash_images/splash_image1.svg","splashBackgroundColor":"#ffffff"}}}
         ) }
     };
