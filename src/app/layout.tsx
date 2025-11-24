c0c49b95f47abe8289cdddc6ae7a538ae118d72e
Akhmad Khudri <khudri@binadarma.ac.id>
2025-11-24 22:43:24 +0700

    Update metadata title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,6 @@ export default function RootLayout({
 }
 
 export const metadata: Metadata = {
-        title: "Vercel Deployment Fixer",
-        description: "Resolve Vercel deployment errors with missing module hooks and optimize your build process for seamless Next.js app updates and fixes.",
-          other: { "fc:frame": JSON.stringify({"version":"next","imageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/thumbnail_miniapp_cmid40lk90kww0arq8hmofhcd-yqJsWWuIdKIYs4QsdfWO5iDpS34TDF","button":{"title":"ELPEEF","action":{"type":"launch_frame","name":"Vercel Deployment Fixer","url":"https://between-method-154.app.ohara.ai","splashImageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/farcaster/splash_images/splash_image1.svg","splashBackgroundColor":"#ffffff"}}}
-        ) }
+        title: "RACE SENSE",
+        description: "AI-Powered Racing Analytics"
     };
